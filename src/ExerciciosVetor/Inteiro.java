27913c24d84073d9796551f99fbda41106e8a634
gabster <101844142+niyjn@users.noreply.github.com>
2026-02-27 13:43:31 -0300

    little detalhe adicionado
    detalhe: OO

--- src/ExerciciosVetor/Inteiro.java
+++ src/ExerciciosVetor/Inteiro.java
@@ -3,68 +3,61 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
+class Vetor {
+    
+    private ArrayList<Integer> valores = new ArrayList<>();
+    
+    public void adicionar(int n) {
+        this.valores.add(n);
+    }
+    
+    public void exibirSoma() {
+        int sum = 0;
+        for(int n : valores) sum += n;
+        System.out.println("Soma: " + sum);   
+    }
+    
+    public void acharMaior() {
+        int maior = valores.get(0);
+        
+        for(int i = 1; i < valores.size(); i++) {
+            int temp = valores.get(i);
+            if (temp > maior) maior = temp;
+        }
+        
+        System.out.println("Maior numero: " + maior);
+    }
+}
+
 public class Inteiro {
+    
     /*Crie um programa que leia 5 números inteiros,
 armazene-os em um vetor e, ao final, mostre a soma
 total e qual foi o maior número digitado.*/
 
     public static void main(String[] args) {
-        ArrayList<Integer> vetor = new ArrayList<>();
+        
+        Vetor vetor = new Vetor();
         Scanner sfc = new Scanner(System.in);
-
-        System.out.println("Digite 5 valores: ");
-            
-        addValor(vetor, sfc);
-
-        somar(vetor);
-
-        acharMaior(vetor);
-
-    }
-
-    private static void somar(ArrayList<Integer> vetor) {
-        int sum = 0;
-        for(Integer bernardo : vetor) {
-            sum += bernardo;
-        }
-        System.out.println("Soma: " + sum);
-
-    }
-
-    private static void addValor(ArrayList<Integer> vetor, Scanner sfc) {
+        System.out.println("Digite 5 números separados por espaço.");
+        
         for(int i = 0; i < 5; i++) {
-            boolean burro = false;
+            boolean userBurro = false;
             
-            while(!burro) {
-                try {
-                int numero = sfc.nextInt();
-                vetor.add(numero);
-                burro = true;
-                System.out.println("Valor adicionado: "+ vetor.get(i));
-                } catch (InputMismatchException e) {
-                    System.out.println("O valor não é valido.");
-                    sfc.nextLine();
+            while(!userBurro) {
+                
+            try{
+            vetor.adicionar(sfc.nextInt()); 
+            userBurro = true;
+            } catch (Exception e) {
+                System.out.println("A entrada deve ser um inteiro.");
+                sfc.nextLine();
                 }
-            }
-        
-        } 
-        
-    }
-    
-    private static void acharMaior(ArrayList<Integer> vetor) {
-        if(vetor.isEmpty()) return;
-        
-        int maior = vetor.get(0);
-        
-        for (int i = 1; i < vetor.size(); i++) {
-            int atual = vetor.get(i);
-            
-            if(atual > maior) {
-                maior = atual;
-            }
+            }            
         }
         
-        System.out.println("O maior numero é: " + maior);
+        vetor.exibirSoma();
+        vetor.acharMaior();
     }
 
 
